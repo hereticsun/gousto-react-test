6c453f6e94c913518b85840e14d088f5b0eb919c
Azlan Cuttilan <azlan@cuttilan.co.uk>
2019-06-19 09:46:15 +0100

    Cleanup

--- src/components/Products/__tests__/Products.test.js
+++ src/components/Products/__tests__/Products.test.js
@@ -24,10 +24,46 @@ describe(`<Products />`, () => {
       };
       const element = shallow(<Products {...props} />);
 
-      it('displays the list of product titles', () => {
+      it('displays correctly', () => {
         expect(element).toMatchSnapshot();
+        expect(element.find('h2').text()).toEqual('Products');
+        expect(element.find('SearchBar')).toBeDefined();
         expect(element.find('Product')).toHaveLength(props.products.length);
       });
+    });
+
+    describe('when I have selected a category', () => {
+      const props = {
+        products: [
+          {
+            id: '1',
+            title: 'Borsao Macabeo',
+            categories: [{id: 1}],
+          },
+          {
+            id: '2',
+            title: 'Love Shortie All Butter Shortbread',
+            categories: [],
+          },
+        ],
+        selectedCategory: {id: 1, title: 'My Category'},
+        fetchProducts: jest.fn()
+      };
+      const element = shallow(<Products {...props} />);
+
+      it('displays products filtered by the selected category', () => {
+        expect(element.find('h2').text()).toEqual('Products in My Category');
+        expect(element.find('Product')).toHaveLength(1);
+        expect(element.find('Product').prop('product')).toEqual(
+          {
+            id: '1',
+            title: 'Borsao Macabeo',
+            categories: [{id: 1}],
+          },
+
+        );
+      });
+
     });
   });
 });
